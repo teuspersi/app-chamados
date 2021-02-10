cfb89aeb0c323631ffea9fdbc93829249ac7216a
Mateus Pereira Silva <56566635+teuspersi@users.noreply.github.com>
2021-02-10 12:51:53 -0300

    Primeiro Commit

---
+++ principal/index.php
@@ -0,0 +1,74 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Abertura de Chamados</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style>
+      .card-login {
+        padding: 30px 0 0 0;
+        width: 350px;
+        margin: 0 auto;
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        Abertura de Chamados
+      </a>
+    </nav>
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-login">
+          <div class="card">
+            <div class="card-header">
+              Login
+            </div>
+            <div class="card-body">
+              <form action="valida_login.php" method="post">
+                <div class="form-group">
+                  <input name="email" type="email" class="form-control" placeholder="E-mail">
+                </div>
+                <div class="form-group">
+                  <input name="senha" type="password" class="form-control" placeholder="Senha">
+                </div>
+
+                <? if(isset($_GET['login']) && $_GET['login'] == 'erro') { ?>
+
+                  <div class="alert alert-danger" role="alert">
+                    Usuário ou senha inválido(s).
+                  </div>
+
+                <? } ?>
+
+                <? if(isset($_GET['login']) && $_GET['login'] == 'erro2') { ?>
+
+                  <div class="alert alert-danger" role="alert">
+                    Faça login antes de acessar as páginas protegidas.
+                  </div>
+                  
+                <? } ?>
+
+                <? if(isset($_GET['login']) && $_GET['login'] == 'logoff') { ?>
+
+                  <div class="alert alert-info" role="alert">
+                    Saiu com sucesso.
+                  </div>
+                  
+                <? } ?>
+
+                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
+  </body>
+</html>
---
+++ principal/valida_login.php
@@ -0,0 +1,4 @@
+<?php
+
+	require "../validacao/valida_login.php";
+?>
